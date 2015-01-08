9431133307aa2cadaae39be7f8bbd48a7c53548b
Jonas Baumann <jone@jone.ch>
2015-01-08 12:21:17 +0100

    Update workflows for Plone 4.3.4.

---
+++ onegov/edu/upgrades/20150108121606_update_workflows/upgrade.py
@@ -0,0 +1,12 @@
+from ftw.upgrade import UpgradeStep
+
+
+class UpdateWorkflows(UpgradeStep):
+    """Update workflows.
+    """
+
+    def __call__(self):
+        self.install_upgrade_profile()
+        self.update_workflow_security(['onegov_edu_intranet_workflow',
+                                       'onegov_edu_workflow'],
+                                      reindex_security=False)
